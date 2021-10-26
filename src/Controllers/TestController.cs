391725bfeb83b58f6fab50083f64c85d0ab7141a
OricoGrillo <leoniinet2005_13@hotmail.com>
2021-10-26 11:46:00 -0500

    first commit

---
+++ src/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace cart_api_gcp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public TestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(_configuration.GetSection("Messages")["Message"]);
+        }
+    }
+}
